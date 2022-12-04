39788acf8d63212ac7fd1d04cf17c37c06eb92ac
anton-karlovskiy <antonkarlovskiy@outlook.com>
2022-12-04 07:31:17 +0100

    chore: clean up

--- examples/_dev/src/components/SendStablecoinDemo/index.tsx
+++ examples/_dev/src/components/SendStablecoinDemo/index.tsx
@@ -11,22 +11,18 @@ import { parseUnits } from '@ethersproject/units';
 import { avalanche } from '../../config/chains';
 
 const ETHEREUM_USDC_CONTRACT_ADDRESS = '0xA0b86991c6218b36c1d19D4a2e9Eb0cE3606eB48';
-// ray test touch <
 const ETHEREUM_USDT_CONTRACT_ADDRESS = '0xdAC17F958D2ee523a2206206994597C13D831ec7';
-// ray test touch >
 const AVALANCHE_USDC_CONTRACT_ADDRESS = '0xB97EF9Ef8734C71904D8002F8b6Bc66Dd9c48a6E';
 const RECIPIENT_ADDRESS = '0x0E63934079EDA886822CA589e0e533ea7440948E';
 const USDC_AMOUNT = '0.01';
 const USDC_DECIMALS = 6;
 
-// ray test touch <
 enum Stablecoin {
   USDC = 'USDC',
   USDT = 'USDT'
 }
 
 const stableCoin = Stablecoin.USDT;
-// ray test touch >
 
 // MEMO: inspired by https://wagmi.sh/examples/contract-write
 const SendStablecoinDemo = () => {
@@ -34,7 +30,6 @@ const SendStablecoinDemo = () => {
 
   let stablecoinContractAddress;
   if (selectedChain?.id === chain.mainnet.id) {
-    // ray test touch <
     if (stableCoin === Stablecoin.USDT) {
       stablecoinContractAddress = ETHEREUM_USDT_CONTRACT_ADDRESS;
     } else if (stableCoin === Stablecoin.USDC) {
@@ -42,7 +37,6 @@ const SendStablecoinDemo = () => {
     } else {
       stablecoinContractAddress = undefined;  
     }
-    // ray test touch >
   } else if (selectedChain?.id === avalanche.id) {
     stablecoinContractAddress = AVALANCHE_USDC_CONTRACT_ADDRESS;
   } else {
