fd029ecf894aca005ee614f28c4160c74216896c
anton-karlovskiy <antonkarlovskiy@outlook.com>
2022-12-21 02:44:32 +0100

    feat: set pageExtensions

--- examples/_dev/next.config.js
+++ examples/_dev/next.config.js
@@ -6,4 +6,5 @@ const withPreconstruct = require('@preconstruct/next')
 /** @type {import('next').NextConfig} */
 module.exports = withPreconstruct({
   reactStrictMode: true,
+  pageExtensions: ['page.tsx', 'page.ts']
 })
