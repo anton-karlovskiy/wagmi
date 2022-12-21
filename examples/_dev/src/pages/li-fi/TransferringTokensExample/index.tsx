525c322ecb039fb59986077145d0053e886ed5f9
anton-karlovskiy <antonkarlovskiy@outlook.com>
2022-12-21 03:13:58 +0100

    feat: add usePrepareSendTransaction

--- examples/_dev/src/pages/li-fi/TransferringTokensExample/index.tsx
+++ examples/_dev/src/pages/li-fi/TransferringTokensExample/index.tsx
@@ -1,6 +1,15 @@
-import { useAccount } from 'wagmi';
+import {
+  useAccount,
+  // ray test touch <
+  useSendTransaction,
+  usePrepareSendTransaction
+  // ray test touch >
+} from 'wagmi';
 import axios from 'axios'; // TODO: use `fetch` API
 import { useQuery } from '@tanstack/react-query';
+// ray test touch <
+import { TransactionRequest } from '@ethersproject/providers';
+// ray test touch >
 
 const FROM_CHAIN = 'DAI';
 const FROM_TOKEN = 'USDC';
@@ -8,24 +17,24 @@ const TO_CHAIN = 'POL';
 const TO_TOKEN = 'USDC';
 const FROM_AMOUNT = '1000000';
 
-const QUOTE_API_ENDPOINT = 'https://li.quest/v1/quote';
+const LIFI_QUOTE_API_ENDPOINT = 'https://li.quest/v1/quote';
 
 const TransferringTokensExample = () => {
   const account = useAccount();
 
   const {
-    isLoading,
-    error,
-    data,
-    isFetching
+    isLoading: quoteLoading,
+    error: quoteError,
+    data: quoteData,
+    isFetching: quoteFetching
   } = useQuery({
     queryKey: [
-      QUOTE_API_ENDPOINT,
+      LIFI_QUOTE_API_ENDPOINT,
       account.address
     ],
     queryFn: () =>
       axios
-        .get(QUOTE_API_ENDPOINT, {
+        .get(LIFI_QUOTE_API_ENDPOINT, {
           params: {
             fromChain: FROM_CHAIN,
             toChain: TO_CHAIN,
@@ -35,20 +44,31 @@ const TransferringTokensExample = () => {
             fromAddress: account.address
           }
         })
-        .then((res) => res.data),
+        // ray test touch <
+        .then((res) => res.data as {
+          transactionRequest: TransactionRequest & { to: string; }
+        }),
+        // ray test touch >
       enabled: !!account.address
   });
   // ray test touch <
-  console.log('ray : ***** data => ', data);
+  console.log('ray : ***** quoteData => ', quoteData);
   // ray test touch >
 
-  if (isLoading) return <div>Loading...</div>;
+  // ray test touch <
+  const { config } = usePrepareSendTransaction({
+    request: quoteData?.transactionRequest
+  });
+  console.log('ray : ***** config => ', config);
+  // ray test touch >
 
-  if (error) return <div>{'An error has occurred: ' + (error instanceof Error ? error.message : JSON.stringify(error))}</div>;
+  if (quoteLoading) return <div>Loading...</div>;
+
+  if (quoteError) return <div>{'An error has occurred: ' + (quoteError instanceof Error ? quoteError.message : JSON.stringify(quoteError))}</div>;
 
   return (
     <div>
-      <div>{isFetching ? 'Updating...' : ''}</div>
+      <div>{quoteFetching ? 'Updating...' : ''}</div>
     </div>
   );
 };
